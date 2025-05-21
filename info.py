86e3b50c52eee933e3b12cb8bb95ba200c804e33
jonysins2211 <Jonysins11220@gmail.com>
2025-05-21 20:44:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -172,15 +172,15 @@ MY_USERS = (my_users) if my_users else [] #input the id of that users who can sh
 NO_PORT = bool(environ.get('NO_PORT', False))
 APP_NAME = None
 if 'DYNO' in environ:
+#if 'DYNO' in environ:
     ON_HEROKU = True
     APP_NAME = environ.get('APP_NAME')
 else:
-    ON_HEROKU = True
+    ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-#FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else (APP_NAME or '') + '.herokuapp.com'
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://lokieztra1993-eeaf20eb540c.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://lokieztra1993-eeaf20eb540c.herokuapp.com/".format(FQDN, PORT)
+URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "http://{}:{}/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -191,7 +191,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
     APP_NAME = str(getenv('APP_NAME'))
 else:
-    ON_HEROKU = True
+    ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 ADMINS.append(5965340120)
 if HAS_SSL:
